751503620ae4f66812c2d811ee4a23d044de3a9e
chrisforrette <chris@chrisforrette.com>
2018-02-04 16:14:24 -0800

    Adding database URL to circleCI config, reducing branch coverage threshold

--- app/models/index.js
+++ app/models/index.js
@@ -1,19 +1,9 @@
 const sequelize = require('../clients/sequelize')
 
-// Build model object
-
-const models = {
+/**
+ * Model object
+ * @type {object}
+ */
+module.exports = {
   FoodTruck: sequelize.import('./food_truck')
 }
-
-// Apply relationships
-
-Object
-  .keys(models)
-  .forEach(function (modelName) {
-    if ('associate' in models[modelName]) {
-      models[modelName].associate(models)
-    }
-  })
-
-module.exports = models
